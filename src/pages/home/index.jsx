4ffb7bcd7fbea8d060559c264acc545e5b401843
Ravikumar <ravikumar@Ravikumars-MacBook-Pro.local>
2020-11-23 04:00:10 +0530

    Application setup and used styled component

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppContainer from './styled'
+
+export default function Home() {
+  return (
+    <AppContainer>
+      <header>Header</header>
+      <main>Main content</main>
+      <footer>Footer</footer>
+    </AppContainer>
+  )
+}
---
+++ src/pages/home/styled/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  margin: 0 auto;
+  text-align: center
+`
+
+export default AppContainer;
