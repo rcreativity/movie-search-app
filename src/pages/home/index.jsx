5127a3a7ec0051b8e3466d07ecd2e9a2da9bf9a5
Ravikumar <ravikumar@Ravikumars-MacBook-Pro.local>
2020-11-23 08:50:19 +0530

    Favourite functionality completed

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,16 +6,18 @@ import Footer from '../../components/footer'
 import MovieList from '../movie-list'
 
 export default function Home() {
+
   return (
     <AppContainer>
       <Header />
+      <h1>Movie Search Application</h1>
       <MainContainer>
         <Switch>
             <Route exact path="/">
               <MovieList />
             </Route>
             <Route path="/favourites">
-              <h2>Favorites</h2>
+              <MovieList />
             </Route>
         </Switch>
       </MainContainer>
--- src/pages/movie-list/index.jsx
+++ src/pages/movie-list/index.jsx
@@ -4,7 +4,7 @@ import { MovieContext } from '../../movieProvider'
 import Search from '../../components/search'
 import MovieCard from '../../components/movieCard'
 
-export default function MovieList() {
+export default function MovieList(props) {
   const { state, dispatch } = useContext(MovieContext);
   const [values, setValues] = useState({
     search: "spider",
@@ -39,22 +39,33 @@ export default function MovieList() {
     });
   };
 
-  console.log(state)
-  const { error, results, loading } = state;
+  const { error, loading } = state;
+  let { results } = state;
+  const path = window.location.pathname;
+  const getFavorites = JSON.parse(localStorage.getItem("favorites"));
+
+  console.log(getFavorites, results);
+
+  if(path === '/favourites'){
+    results = results.filter(val => getFavorites.includes(val.imdbID));
+    console.log(results)
+  }
 
   return (
     <MovieListContainer>
-      <Search 
-        values={values} 
-        submitForm={submitForm} 
-        handleChange={handleChange} 
-      />
+      {path === '/' && (
+        <Search 
+          values={values} 
+          submitForm={submitForm} 
+          handleChange={handleChange} 
+        />
+      )}
       {loading && 'loading'}
       {error && (
         <ErrorMessage>Something went wrong, Please search with valid name</ErrorMessage>
       )}
       <MovieListUl>
-        {!loading && results?.map((value)=> <MovieCard key={value?.imdbID} movie={value} /> )}
+        {!loading && results?.map((value)=> <MovieCard path={path} key={value?.imdbID} movie={value} /> )}
       </MovieListUl>
     </MovieListContainer>
   )
